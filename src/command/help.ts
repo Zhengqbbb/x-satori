c651c7a2741c455b038aef7a55c3014c213d148e
Zhengqbbb <1074059947@qq.com>
2024-07-12 11:15:39 +0800

    docs: update README

--- src/command/help.ts
+++ src/command/help.ts
@@ -5,7 +5,7 @@ export function generateHelp(version: string, code = 0) {
     // prettier-ignore
     console.log(
         `${style.yellow('NAME:')}
-    ${style.green('x-satori')} - use Vue files to generate SVG images using Satori
+    ${style.green('x-satori')} - use Vue or Astro file to generate SVG images using Satori
 
 ${style.yellow('WEBSITE:')}
     ${style.underline('https://github.com/Zhengqbbb/x-satori')}
@@ -13,17 +13,17 @@ ${style.yellow('WEBSITE:')}
 ${style.yellow('VERSION:')} ${version}
 
 ${style.yellow('SYNOPSIS:')}
-    x-satori --template <vue_file_path> --config <satori_config_path> [ --output <path> | --dev ]
+    x-satori --template <template_file_path> --config <satori_config_path> [ --output <path> | --dev ]
 
 ${style.yellow('OPTIONS:')}
     ${style.cyan('-d|--dev')}                   ${style.red('Turn on Dev mode')}
-    ${style.cyan('-t|--template <path>')}       ${style.red('The Vue template file path')}
+    ${style.cyan('-t|--template <path>')}       ${style.red('The Vue or Astro template file path')}
     ${style.cyan('-c|--config   <path>')}       ${style.red('The export satori configure file path')}
     ${style.cyan('-o|--output   <path>')}       ${style.red('Target output SVG path')}
 
 ${style.yellow('EXAMPLES:')}
     ${style.cyan('x-satori --config "./satori.js" --template "./Template.vue" --dev')}
-    ${style.cyan('x-satori --config "./satori.js" --template "./Template.vue"')}
+    ${style.cyan('x-satori --config "./satori.js" --template "./Template.astro"')}
     ${style.cyan('x-satori --config "./satori.js" --template "./Template.vue" -o image.svg')}
 `,
     )
